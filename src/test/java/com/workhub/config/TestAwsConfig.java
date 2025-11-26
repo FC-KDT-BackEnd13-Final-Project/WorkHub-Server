b33436c18a65c355f066fbe9bcf7d8c530ee83f9
JWoong-01 <yju407@naver.com>
2025-11-26 20:18:14 +0900

    fix: ci 오류 수정

--- src/test/java/com/workhub/config/TestAwsConfig.java
+++ src/test/java/com/workhub/config/TestAwsConfig.java
@@ -1,5 +1,6 @@
 package com.workhub.config;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -12,5 +13,10 @@ public class TestAwsConfig {
     @Bean
     public S3Client s3Client() {
         return mock(S3Client.class);
+    }
+
+    @Bean
+    public Flyway flyway() {
+        return mock(Flyway.class);
     }
 }
