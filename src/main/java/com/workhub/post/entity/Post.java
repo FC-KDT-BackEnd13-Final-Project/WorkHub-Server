32289fd3a5b36c3406d49f9da48a58d5e5398e98
JWoong-01 <yju407@naver.com>
2025-12-01 14:48:29 +0900

    refactor: soft delete, deleted_at이 null인 값만 조회

--- src/main/java/com/workhub/post/entity/Post.java
+++ src/main/java/com/workhub/post/entity/Post.java
@@ -10,7 +10,6 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
-import org.hibernate.annotations.Where;
 
 import java.time.LocalDateTime;
 
