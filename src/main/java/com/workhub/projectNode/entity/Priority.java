8850bdf1a3361196d16ad85a444ca2543b6e19ff
CHO GUNHEE <lan0184@naver.com>
2025-12-02 14:43:54 +0900

    feat : 프로젝트 노드 생성 구현.

---
+++ src/main/java/com/workhub/projectNode/entity/Priority.java
@@ -0,0 +1,18 @@
+package com.workhub.projectNode.entity;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+public enum Priority {
+    LOW, MEDIUM, HIGH, CRITICAL;
+
+    @JsonCreator
+    public static Priority from(String value) {
+        if (value == null) return null;
+        return Priority.valueOf(value.toUpperCase());
+    }
+
+    @JsonCreator
+    public String to(String value) {
+        return this.name();
+    }
+}
